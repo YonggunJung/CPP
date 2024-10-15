0a227a633881a945d9135487c08c937a47947b7c
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-10-15 15:44:36 +0900

    Add files via upload

---
+++ Lecture/main.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+int main() {
+	int favorite_number;
+
+	std::cout << "1과 100 사이에서 가장 좋아하는 숫자를 입력하세요~: ";
+	std::cin >> favorite_number;
+	std::cout << "Amazing!! that's my favorite number too!" << std::endl;
+
+	return 0; //optional
+}
